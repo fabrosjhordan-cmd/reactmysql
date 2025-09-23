a35d54958ee52970a1f13a6d0309e9c2150256d2
fabrosjhordan-cmd <fabrosjhordan@gmail.com>
2025-09-23 19:43:01 +0800

    ReactJS and MySQL (Display and Insert)  in one folder (Complete)

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'testreact'
+})
+
+app.get('/', (req, res)=>{
+    return res.json('from backend');
+})
+
+app.get('/user', (req, res)=>{
+    const sql = "SELECT * FROM user";
+    db.query(sql, (err, data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+app.post('/user', (req, res)=>{
+    const sql = "INSERT INTO user (`email`, `username`, `password`) VALUES (?)";
+    const values = [
+        req.body.email,
+        req.body.username,
+        req.body.password
+    ];
+
+    db.query(sql, [values], (err, data)=>{
+        if (err) return res.json(err);
+        return res.json({message: "User added successfully", id: data.insertId})
+    })
+})
+
+app.listen(8081, ()=>{
+    console.log('listening')
+})
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+  const [form, setForm] = useState({
+    email: '',
+    username: '',
+    password: '',
+  });
 
+  useEffect(() => {
+    fetch('http://localhost:8081/user')
+    .then(res=> res.json())
+    .then(data => setData(data))
+    .catch(err => console.log(err));
+  }, []);
+
+  const handleChange = (e) =>{
+    setForm({ ...form, [e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = () =>{
+    fetch('http://localhost:8081/user',{
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(form)
+    })
+    .then(res => res.json())
+    .then(result =>{
+      console.log(result);
+      return fetch('http://localhost:8081/user')
+      .then(res => res.json())
+      .then(data => setData(data));
+    })
+    .catch(err => console.log(err));
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{padding: '50px'}}>
+        <table>
+          <thead>
+            <th>Id</th>
+            <th>Email</th>
+            <th>Username</th>
+            <th>Password</th>
+          </thead>
+          <tbody>
+            {data.map((d, i) =>(
+              <tr key={i}>
+                <td>{d.id}</td>
+                <td>{d.email}</td>
+                <td>{d.username}</td>
+                <td>{d.password}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      <h3 style={{ marginTop: "20px" }}>Add New User</h3>
+      <input
+        type="text"
+        name="email"
+        placeholder="Email"
+        onChange={handleChange}
+        style={{ width: "100%", margin: "2px 0" }}
+      />
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        onChange={handleChange}
+        style={{ width: "100%", margin: "2px 0" }}
+      />
+      <input
+        type="text"
+        name="password"
+        placeholder="Password"
+        onChange={handleChange}
+        style={{ width: "100%", margin: "2px 0" }}
+      />
+
+      <button type="button" onClick={handleSubmit}>
+        Submit
+      </button>
+    </div>
   )
 }
 
